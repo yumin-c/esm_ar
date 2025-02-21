5a971fc786b0b2a0db6ea3c414b846c0bec98114
yumin-c <yumin.c@yonsei.ac.kr>
2025-02-21 12:35:23 +0900

    init commit

---
+++ preprocessing.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from Bio.Seq import Seq
+
+data = pd.read_csv('/extdata2/YMC/AR/data/AR_Dependency_Result3_syn5.csv')
+refseq = 'atggaagtgcagttagggctgggaagggtctaccctcggccgccgtccaagacctaccgaggagctttccagaatctgttccagagcgtgcgcgaagtgatccagaacccgggccccaggcacccagaggccgcgagcgcagcacctcccggcgccagtttgctgctgctgcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcagcaagagactagccccaggcagcagcagcagcagcagggtgaggatggttctccccaagcccatcgtagaggccccacaggctacctggtcctggatgaggaacagcaaccttcacagccgcagtcggccctggagtgccaccccgagagaggttgcgtcccagagcctggagccgccgtggccgccagcaaggggctgccgcagcagctgccagcacctccggacgaggatgactcagctgccccatccacgttgtccctgctgggccccactttccccggcttaagcagctgctccgctgaccttaaagacatcctgagcgaggccagcaccatgcaactccttcagcaacagcagcaggaagcagtatccgaaggcagcagcagcgggagagcgagggaggcctcgggggctcccacttcctccaaggacaattacttagggggcacttcgaccatttctgacaacgccaaggagttgtgtaaggcagtgtcggtgtccatgggcctgggtgtggaggcgttggagcatctgagtccaggggaacagcttcggggggattgcatgtacgccccacttttgggagttccacccgctgtgcgtcccactccttgtgccccattggccgaatgcaaaggttctctgctagacgacagcgcaggcaagagcactgaagatactgctgagtattcccctttcaagggaggttacaccaaagggctagaaggcgagagcctaggctgctctggcagcgctgcagcagggagctccgggacacttgaactgccgtctaccctgtctctctacaagtccggagcactggacgaggcagctgcgtaccagagtcgcgactactacaactttccactggctctggccggaccgccgccccctccgccgcctccccatccccacgctcgcatcaagctggagaacccgctggactacggcagcgcctgggcggctgcggcggcgcagtgccgctatggggacctggcgagcctgcatggcgcgggtgcagcgggacccggttctgggtcaccctcagccgccgcttcctcatcctggcacactctcttcacagccgaagaaggccagttgtatggaccgtgtggtggtggtgggggtggtggcggcggcggcggcggcggcggcggcggcggcggcggcggcggcggcggcgaggcgggagctgtagccccctacggctacactcggccccctcaggggctggcgggccaggaaagcgacttcaccgcacctgatgtgtggtaccctggcggcatggtgagcagagtgccctatcccagtcccacttgtgtcaaaagcgaaatgggcccctggatggatagctactccggaccttacggggacatgcgtttggagactgccagggaccatgttttgcccattgactattactttccaccccagaagacctgcctgatctgtggagatgaagcttctgggtgtcactatggagctctcacatgtggaagctgcaaggtcttcttcaaaagagccgctgaagggaaacagaagtacctgtgcgccagcagaaatgattgcactattgataaattccgaaggaaaaattgtccatcttgtcgtcttcggaaatgttatgaagcagggatgactctgggagcccggaagctgaagaaacttggtaatctgaaactacaggaggaaggagaggcttccagcaccaccagccccactgaggagacaacccagaagctgacagtgtcacacattgaaggctatgaatgtcagcccatctttctgaatgtcctggaagccattgagccaggtgtagtgtgtgctggacacgacaacaaccagcccgactcctttgcagccttgctctctagcctcaatgaactgggagagagacagcttgtacacgtggtcaagtgggccaaggccttgcctggcttccgcaacttacacgtggacgaccagatggctgtcattcagtactcctggatggggctcatggtgtttgccatgggctggcgatccttcaccaatgtcaactccaggatgctctacttcgcccctgatctggttttcaatgagtaccgcatgcacaagtcccggatgtacagccagtgtgtccgaatgaggcacctctctcaagagtttggatggctccaaatcaccccccaggaattcctgtgcatgaaagcactgctactcttcagcattattccagtggatgggctgaaaaatcaaaaattctttgatgaacttcgaatgaactacatcaaggaactcgatcgtatcattgcatgcaaaagaaaaaatcccacatcctgctcaagacgcttctaccagctcaccaagctcctggactccgtgcagcctattgcgagagagctgcatcagttcGcttttgacctgctaatcaagtcacacatggtgagcgtggactttccggaaatgatggcagagatcatctctgtgcaagtgcccaagatcctttctgggaaagtcaagcccatctatttccacacccagtga'
+
+data = data[~data['AA_Change'].str.contains('_', na=False)]
+
+# Filtering: stop codons
+data = data[~data['AA_Change'].str.startswith('*', na=False)]
+data = data[~data['AA_Change'].str.endswith('*', na=False)]
+
+data['label'] = data['Fitness_Score'] / 10
+data['classification'] = data['Classification'].apply(lambda x: 1 if x == 'Functional' else 0)
+data = data[['AA_Change', 'classification', 'label']]
+
+print(f'Remaining data count: {len(data)}')
+
+# RefSeq into AA seq
+refseq_dna = Seq(refseq)
+refseq_protein = refseq_dna.translate()
+
+def apply_aa_change(ref_protein, aa_change):
+    mutated_seq = list(str(ref_protein))
+    try:
+        pos = int(aa_change[1:-1]) - 1 # Position (Zero-indexed)
+        if mutated_seq[pos] == aa_change[0]:
+            mutated_seq[pos] = aa_change[-1]
+        return ''.join(mutated_seq[:-1])
+    except:
+        return None
+
+data['wt'] = str(refseq_protein[:-1])
+data['sequence'] = data['AA_Change'].apply(lambda x: apply_aa_change(refseq_protein, x)) # add position
+data['pos'] = data['AA_Change'].apply(lambda x: int(x[1:-1]) if x[1:-1].isdigit() else None)
+
+data = data.dropna()
+
+data.to_csv('/extdata2/YMC/AR/data/filtered_dependency.csv', index=False)
+print("Filtered data saved successfully.")
